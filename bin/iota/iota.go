3d68e375a8e8baff32ade52e721599d86b3a8553
Rich Davis <vgcrld@gmail.com>
2023-06-19 20:04:34 -0400

    updates

--- bin/iota/iota.go
+++ bin/iota/iota.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"math"
+)
 
 // iota is incremented each time it is executed.
 // it only works as a const
@@ -20,4 +23,8 @@ const newIota = iota
 func main() {
 	fmt.Printf("KiB=%v, MiB=%v, GiB=%v, TiB=%v, PiB=%v\n", KiB, MiB, GiB, TiB, PiB)
 	fmt.Println(newIota, "ioto is reset outside of the const")
+	xx := 127.0 / 5.0
+	i, f := math.Modf(xx)
+	fmt.Printf("int=%v, fractional=%v\n", i, f)
+
 }
