4fd70afaab625c2a36cac14f34b5643680992810
Rich Davis <vgcrld@gmail.com>
2025-08-26 06:28:17 -0400

    Refactor: Remove unused reflection code from main.go

--- bin/workers/main.go
+++ bin/workers/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"math/rand"
 	"sync"
@@ -12,22 +13,31 @@ var (
 	ch chan int
 )
 
+// Default values for the application.
 const (
-	threads  = 50
+	threads  = 2
 	values   = 50
 	max_time = 5000 // Maximum sleep time in milliseconds
 )
 
 func main() {
 
+	t := flag.Int("threads", threads, "Number of goroutines to start")
+	v := flag.Int("values", values, "Number of values to process")
+	m := flag.Int("max-time", max_time, "Maximum sleep time in milliseconds")
+
+	flag.Parse()
+
+	fmt.Printf("Starting %d goroutines to process %d values with max sleep time %dms\n", *t, *v, *m)
+
 	ch = make(chan int)
 
-	for i := range threads {
+	for i := range *t {
 		wg.Add(1)
-		go fk(i)
+		go fk(i, *m)
 	}
 
-	for range values {
+	for range *v {
 		ch <- 10 + rand.Intn(41)
 	}
 	close(ch)
@@ -37,10 +47,10 @@ func main() {
 	fmt.Println("Exiting main function")
 }
 
-func fk(tname int) {
+func fk(tname, msecs int) {
 	fmt.Printf("Starting goroutine %d\n", tname)
 	for n := range ch {
-		st := rand.Intn(max_time)
+		st := rand.Intn(msecs)
 		fmt.Printf("go%d Received: %d, sleeptime: %dms\n", tname, n, st)
 		time.Sleep(time.Duration(st) * time.Millisecond)
 	}
